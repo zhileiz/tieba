bef052478577685540607e653db7782a9fc52c0c
zhileiz <zhileiz@seas.upenn.edu>
2017-06-05 11:27:32 -0400

    need migration for user_id

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def xiangce
+  end
+
+  def about
+  end
+
+  def toupiao
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   root "posts#index"
+  get "/about" => "pages#about"
+  get "/toupiao" => "pages#toupiao"
+  get "/xiangce" => "pages#xiangce"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
